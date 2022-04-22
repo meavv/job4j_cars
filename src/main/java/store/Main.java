3e20470d365f7261fa2095b16790682a2b1d4065
meavv <c.c.c.p@bk.ru>
2022-04-22 12:05:15 +0500

    добавил фильтр

--- src/main/java/store/Main.java
+++ src/main/java/store/Main.java
@@ -6,8 +6,6 @@ import model.User;
 public class Main {
 
     public static void main(String[] args) {
-        var s = CarRepository.findAllBrand();
-        System.out.println(s.toString());
 
     }
 
